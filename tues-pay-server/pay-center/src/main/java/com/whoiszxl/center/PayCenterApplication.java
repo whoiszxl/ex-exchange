b9243510316d17d6ffd160836ffa0eb7a2f464fc
whoiszxl <whoiszxl@gmail.com>
2020-12-05 00:45:05 +0800

    init java project

---
+++ tues-pay-server/pay-center/src/main/java/com/whoiszxl/center/PayCenterApplication.java
@@ -0,0 +1,13 @@
+package com.whoiszxl.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayCenterApplication.class, args);
+    }
+
+}
